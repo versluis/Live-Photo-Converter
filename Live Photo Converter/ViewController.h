5baf85727efecc85a137c752fea03c95d4bfa1cf
Jay Versluis <jay@versluis.com>
2015-10-14 07:26:05 -0400

    Initial Commit

---
+++ Live Photo Converter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Live Photo Converter
+//
+//  Created by Jay Versluis on 14/10/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Live Photo ConverterTests/Live_Photo_ConverterTests.m
@@ -0,0 +1,39 @@
+//
+//  Live_Photo_ConverterTests.m
+//  Live Photo ConverterTests
+//
+//  Created by Jay Versluis on 14/10/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Live_Photo_ConverterTests : XCTestCase
+
+@end
+
+@implementation Live_Photo_ConverterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
